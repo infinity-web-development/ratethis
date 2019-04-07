ed9c4661433bb2b16592e92fb990065c92c8415b
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-04-07 15:39:20 +0100

    footer wrapped on all pages

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import stylesheet from 'antd/dist/antd.min.css'
 import { Layout } from 'antd';
 
+import AppFooterLayout from '../components/footer/AppFooterLayout';
 import Home from '../components/home/Home';
 
 export default class App extends React.Component {
     render() {
         return (
             <Layout>
-                <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-                <Home />
+                <AppFooterLayout>
+                    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+                    <Home />
+                </AppFooterLayout>
             </Layout>
         )
     }
