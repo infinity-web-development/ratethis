ae3e2d0b0972285913422429b0b5f47380a88713
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-04-07 16:04:36 +0100

    [fix] minor clean up

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,12 +1,11 @@
-import React from 'react';	
+import React from 'react';
 
- class Home extends React.Component {	
-    render() {	
-        return (	
-            <h1>Hello World!</h1>	
-        );	
-    }	
-}	
+ class Home extends React.Component {
+    render() {
+        return (
+            <h1>Hello World!</h1>
+        );
+    }
+}
 
- export default Home;
- 
+export default Home;
