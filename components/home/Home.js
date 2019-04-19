8fb83e0e8b691e783aeb63fad4e3d6cfc187261d
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-04-19 15:49:55 +0100

    [temporary] click

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,65 +1,78 @@
-import React from 'react';
-import { Avatar, Card, Icon, List } from 'antd';
+import React from "react";
+import { Avatar, Card, Icon, List } from "antd";
 
-import { ICON_LIST, LIST_TEXTS, STYLES, USER_UPLOAD } from './constants';
 import UserUpload from './UserUpload';
+import { ICON_LIST, LIST_TEXTS, STYLES, USER_UPLOAD } from "./constants";
 
 const { AVATAR, CARD_CONTAINER, ICON, USER_LIST } = STYLES;
 const { INNER, MORE, UPLOAD, VERTICAL } = LIST_TEXTS;
 
 class Home extends React.Component {
-    state = {
-        clicks: 0,
-        userUpload: USER_UPLOAD,
-    };
+  state = {
+    clicks: {}
+  };
 
-    IncrementIconText = () => {
-        this.setState({ clicks: this.state.clicks + 1 });
-    };
+  getClickCount = (state, image, type) => {
+    if (!state[image] || !state[image][type]) return 0;
+    return state[image][type];
+  };
 
-    render() {
-        const actions = ICON_LIST.map(({ type }) => (
-            <span>
-                <Icon
-                  key={type}
-                  type={type}
-                  onClick={this.IncrementIconText}
-                  style={ICON}
-                />
-                    {this.state.clicks}
-            </span>
+  incrementIconText = (image, type) => {
+    this.setState(prevState => {
+      const previousClickCount = this.getClickCount(prevState, image, type);
+      return {
+        ...prevState,
+        [image]: {
+          ...prevState[image],
+          [type]: previousClickCount + 1
+        }
+      };
+    });
+  };
 
-        ));
-        return (
-            <div>
-                <UserUpload />
-                <List
-                  itemLayout={VERTICAL}
-                  dataSource={this.state.userUpload}
-                  renderItem={item => (
-                    <List.Item style={USER_LIST}>
-                        <Card
-                          actions={actions}
-                          cover={<img alt={UPLOAD} src={item.image} />}
-                          extra={<Icon type={MORE} />}
-                          hoverable
-                          title={(
-                                <a>
-                                    <Avatar src={item.image} style={AVATAR} />
-                                    {item.user}
-                                </a>
-    )}
-                          type={INNER}
-                          style={CARD_CONTAINER}
-                        >
-                            {item.story}
-                        </Card>
-                    </List.Item>
-                  )}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <UserUpload />
+        <List
+          itemLayout={VERTICAL}
+          dataSource={USER_UPLOAD}
+          renderItem={item => (
+            <List.Item style={USER_LIST}>
+              <Card
+                actions={ICON_LIST.map(({ type }) => {
+                  return (
+                    <span>
+                      <Icon
+                        key={type}
+                        type={type}
+                        onClick={() => this.incrementIconText(item.image, type)}
+                        style={ICON}
+                      />
+                      {this.getClickCount(this.state, item.image, type)}
+                    </span>
+                  );
+                })}
+                cover={<img alt={UPLOAD} src={item.image} />}
+                extra={<Icon type={MORE} />}
+                hoverable
+                title={
+                  <a>
+                    <Avatar src={item.image} style={AVATAR} />
+                    {item.user}
+                  </a>
+                }
+                type={INNER}
+                style={CARD_CONTAINER}
+              >
+                {item.story}
+              </Card>
+            </List.Item>
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default Home;
