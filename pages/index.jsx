b67fa5d8df20a0873de7d3890cc76b382931c33d
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-04-30 22:57:58 +0100

    [add] redux

---
+++ pages/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import stylesheet from 'antd/dist/antd.min.css';
+import { Layout } from 'antd';
+import { connect } from 'react-redux';
+
+import AppFooterLayout from '../components/footer/AppFooterLayout';
+import Home from '../components/home/Home';
+import HeaderLayout from '../components/header/HeaderLayout';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <HeaderLayout>
+                    <AppFooterLayout>
+                        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+                        <Home />
+                    </AppFooterLayout>
+                </HeaderLayout>
+            </Layout>
+        );
+    }
+}
+
+export default connect(state => state)(App);
